eeb0e0b8189a2c8332b5210f6e400ea069883a0e
Sean Bethel <tech@jmbdc.com>
2017-02-21 20:22:12 +0000

    change float class

--- src/resources/views/extras/author.blade.php
+++ src/resources/views/extras/author.blade.php
@@ -1,5 +1,5 @@
 <div class="page_intro clearfix m-b-3">
-<img class="rounded-circle pull-sm-left mr-1" style="max-width: 300px;" src="{{$author->mugshot}}">
+<img class="rounded-circle float-sm-left mr-1" style="max-width: 300px;" src="{{$author->mugshot}}">
 <h3>{{$author->title}} <small>{{$author->department}}</small></h3>
 {!! $author->bio !!}
 </div>
